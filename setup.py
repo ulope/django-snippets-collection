4cd9c93feec5e59e77a1df106cbdff8fe140c99b
Ulrich Petri <u.petri@diefirma.de>
2009-10-09 19:07:55 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name = "django-snippets-collection",
+    version = "0.1",
+    packages = find_packages(),
+    author = "Ulrich Petri",
+    author_email = "mail@ulo.pe",
+    description = "Some useful things from djangosnippets.",
+    long_description = """
+    """,
+    license = "BSD",
+    keywords = "django",
+    url = "http://github.com/ulope/django-snippets-collection",
+)
+
