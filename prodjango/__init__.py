480bb209202ef7df9acd41987b44059397db76a4
Ulrich Petri <u.petri@diefirma.de>
2009-10-09 18:45:21 +0200

    Initial Import

---
+++ prodjango/__init__.py
@@ -0,0 +1,1 @@
+# Taken from Chapter 11 samples of "Pro Django" by Marty Alchin
---
+++ prodjango/current_user/decorators.py
@@ -0,0 +1,5 @@
+from django.utils.decorators import decorator_from_middleware
+
+from snippets.prodjango.current_user.middleware import CurrentUserMiddleware
+
+record_current_user = decorator_from_middleware(CurrentUserMiddleware)
